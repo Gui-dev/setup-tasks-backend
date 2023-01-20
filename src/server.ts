0b724d5a44731725632b9040e48c4f2d71a7e2fe
Gui-dev <adrianon10@gmail.com>
2023-01-20 03:39:02 -0300

    feat: add cors

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,19 @@
 import Fastify from 'fastify'
+import cors from '@fastify/cors'
+
+import { prisma } from './services/prisma'
 
 const app = Fastify()
 const PORT = 3333 || process.env.PORT
 
-app.get('/', () => {
-  return 'Hello World'
+app.register(cors)
+
+app.get('/', async () => {
+  const habits = await prisma.habit.findMany()
+
+  return habits
 })
 
 app.listen({
   port: PORT
-})
+}).then(() => console.log(`Server running at http://localhost:${PORT}`))
