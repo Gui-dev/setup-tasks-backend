93ffe3bc4e353e0414eb596e2abcc635fda64b5b
Gui-dev <adrianon10@gmail.com>
2023-01-23 04:15:20 -0300

    feat: add controller HabitController

---
+++ src/controllers/HabitController.ts
@@ -0,0 +1,16 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { CreateHabit } from '../use-cases/habits/CreateHabit'
+import { createHabitsBody } from '../validations/createHabits'
+
+export class HabitController {
+  public async store (request: FastifyRequest, response: FastifyReply) {
+    const { title, weekDays } = createHabitsBody.parse(request.body)
+    const createHabit = new CreateHabit()
+    await createHabit.execute({
+      title,
+      weekDays
+    })
+
+    return response.code(201)
+  }
+}
