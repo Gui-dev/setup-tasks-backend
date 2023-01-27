3682c4ea95f78f5afc33b33a267da0fe95ad7818
Gui-dev <adrianon10@gmail.com>
2023-01-27 03:26:06 -0300

    feat: add helper method update

--- src/controllers/HabitController.ts
+++ src/controllers/HabitController.ts
@@ -1,6 +1,9 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
+
 import { CreateHabit } from '../use-cases/habits/CreateHabit'
+import { ToggleHabit } from '../use-cases/habits/ToggleHabit'
 import { createHabitsBody } from '../validations/createHabits'
+import { toggleHabitParams } from '../validations/toggleHabit'
 
 export class HabitController {
   public async store (request: FastifyRequest, response: FastifyReply): Promise<FastifyReply> {
@@ -12,5 +15,13 @@ export class HabitController {
     })
 
     return response.code(201)
+  }
+
+  public async update (request: FastifyRequest, response: FastifyReply): Promise<FastifyReply> {
+    const { id } = toggleHabitParams.parse(request.params)
+    const toggleHabit = new ToggleHabit()
+    await toggleHabit.execute({ id })
+
+    return response.code(201).send()
   }
 }
