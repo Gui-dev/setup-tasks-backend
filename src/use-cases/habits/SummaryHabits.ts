67aa6aa1397d67b75285a69f7e99430bb6d385a6
Gui-dev <adrianon10@gmail.com>
2023-01-27 04:05:31 -0300

    feat: add use case SummaryHabits

---
+++ src/use-cases/habits/SummaryHabits.ts
@@ -0,0 +1,39 @@
+import { prisma } from '../../services/prisma'
+
+interface ISummaryHabitsResponse {
+  summary: Array<{
+    id: string,
+    date: Date,
+    completed: number,
+    amount: number
+  }>
+}
+
+export class SummaryHabits {
+  public async execute (): Promise<ISummaryHabitsResponse[]> {
+    const summaries: ISummaryHabitsResponse[] = await prisma.$queryRaw`
+      SELECT
+        D.id,
+        D.date,
+        (
+          SELECT
+            cast(count(*) as float)
+          FROM day_habits DH
+          WHERE DH.day_id = D.id
+        ) as completed,
+        (
+          SELECT
+            cast(count(*) as float)
+          FROM habit_week_days HWD
+          JOIN habits H
+            ON H.id = HWD.habit_id
+          WHERE
+            HWD.week_day = cast(strftime('%w', D.date/1000.0, 'unixepoch') as int)
+            AND H.created_at <= D.date
+        ) as amount
+      FROM days D
+    `
+
+    return summaries
+  }
+}
