2dac795305618eac89139ef74891ea54420141ac
Gui-dev <adrianon10@gmail.com>
2023-01-27 04:05:53 -0300

    feat: add helper method index

--- src/controllers/HabitController.ts
+++ src/controllers/HabitController.ts
@@ -1,11 +1,18 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 
 import { CreateHabit } from '../use-cases/habits/CreateHabit'
+import { SummaryHabits } from '../use-cases/habits/SummaryHabits'
 import { ToggleHabit } from '../use-cases/habits/ToggleHabit'
 import { createHabitsBody } from '../validations/createHabits'
 import { toggleHabitParams } from '../validations/toggleHabit'
 
 export class HabitController {
+  public async index (request: FastifyRequest, response: FastifyReply): Promise<FastifyReply> {
+    const summaryHabits = new SummaryHabits()
+    const summaries = await summaryHabits.execute()
+    return response.code(201).send({ summaries })
+  }
+
   public async store (request: FastifyRequest, response: FastifyReply): Promise<FastifyReply> {
     const { title, weekDays } = createHabitsBody.parse(request.body)
     const createHabit = new CreateHabit()
